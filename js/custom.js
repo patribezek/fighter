b826d9482e3af565a9b81d1b747591827fb1fb32
patribezek <patribezek@gmail.com>
2016-10-25 12:12:45 -0300

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+
+	$(".flipme").click(function(){
+		$(".front").addClass("hide");
+		$(".back").removeClass("hide");
+	});
+
+$(".choose").click(function(){
+		$(".back").addClass("hide");
+		$(".payment").removeClass("hide");
+	});
+
+
+
+})
