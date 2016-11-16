6e8547cc1c9999aeff9c373179c4725f8ca3d6f9
patribezek <patribezek@gmail.com>
2016-11-16 11:43:39 -0300

    Add files via upload

---
+++ scripts.php
@@ -0,0 +1,31 @@
+<!-- ARCHIVOS Y LIBS JavaScript -->
+	<!-- jQuery -->
+    	<script src="js/jquery.js"></script>
+    	<script src="js/bootstrap.min.js"></script>
+
+
+
+
+<!-- CUSTOM -->
+   	<script src="js/custom.js"></script>
+
+<!--  ARCHIVOS Y LIBS CSS -->
+
+<!-- Bootstrap Core CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+
+<!-- HTML5 Shim and Respond.js IE8 su pport of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+        <link href="css/materialdesignicons.min.css" media="all" rel="stylesheet" type="text/css" />
+        <!-- CUSTOM -->
+        <link rel="stylesheet" type="text/css" href="styles.css">
+        <link href="https://fonts.googleapis.com/css?family=Boogaloo|Kaushan+Script" rel="stylesheet">
+
+        <link href="css/thumbnail-gallery.css" rel="stylesheet">
+        <link rel="icon" href="img/favicon.ico" type="image/x-icon" />
+        <link rel="stylesheet" type="text/css" href="css/custom.css">
+        <meta name="keywords" content="html, psd, image, web, simple">
